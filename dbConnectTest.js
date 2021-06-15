f0390ff76189eed0f930b56caa307bc5f1d89773
손모함 <73145656+SonMoHam@users.noreply.github.com>
2021-06-15 11:31:58 +0900

    Update dbConnectTest.js

--- dbConnectTest.js
+++ dbConnectTest.js
@@ -4,4 +4,21 @@ const sequelize = new Sequelize('son', 'admin', 'cometrue', {
     host: 'database-2.cb0teqnptu4h.ap-northeast-2.rds.amazonaws.com'
 });
 
-const Op = Sequelize.Op;
+function connectTest() {
+    console.log('Sequelize connect example3');
+    const sequelize = new Sequelize('son', 'admin', 'cometrue', {
+        dialect: 'mysql',
+        host: 'database-2.cb0teqnptu4h.ap-northeast-2.rds.amazonaws.com'
+    });
+    
+    sequelize.authenticate()
+    .then(() => {
+        console.log('Sequelize DB 연결 성공');
+        sequelize.close();
+    })
+    .catch(err => {
+        console.error('Sequelize DB 연결 실패 :', err);
+    });  
+}
+
+connectTest();
