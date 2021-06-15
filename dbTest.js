103a90fd55b99b986a377ad1f9ff20d8b48770d9
손모함 <73145656+SonMoHam@users.noreply.github.com>
2021-06-15 11:48:47 +0900

    db 테스트
    연결 및 기타 테스트

--- dbTest.js
+++ dbTest.js
@@ -4,13 +4,33 @@ const sequelize = new Sequelize('son', 'admin', 'cometrue', {
     host: 'database-2.cb0teqnptu4h.ap-northeast-2.rds.amazonaws.com'
 });
 
+class TestTable extends Sequelize.Model {}
+TestTable.init(
+    {
+        content: {
+            type: Sequelize.STRING,
+            allowNull: true
+        }
+    },
+    {
+        sequelize,
+        charset: 'utf8',
+        collate: 'utf8_general_ci'
+    }
+);
+
+const prepareModel = async () => {
+    try {
+        await TestTable.sync({force:true});
+        sequelize.close();
+    } catch (error) {
+        console.log('TestTable.sync Error ', error);
+    }
+}
+
 function connectTest() {
     console.log('Sequelize connect example3');
-    const sequelize = new Sequelize('son', 'admin', 'cometrue', {
-        dialect: 'mysql',
-        host: 'database-2.cb0teqnptu4h.ap-northeast-2.rds.amazonaws.com'
-    });
-    
+
     sequelize.authenticate()
     .then(() => {
         console.log('Sequelize DB 연결 성공');
